eb3ab9e285739d653688822cc63bd46ee5067722
Ian Taylor <ian@iantaylor.xyz>
2019-08-08 17:37:23 -0600

    Finished add_team
    TODO: leagueid and seasonid parameters

--- efl/data/eflgames.py
+++ efl/data/eflgames.py
@@ -2,9 +2,7 @@
 
 from . import engine
 from . import orm
-
-import sqlalchemy
-import pandas
+import sqlalchemy as sa
 
 class EFLGames(object):
     """Class representing a read-only view of a subset of EFL games."""
@@ -16,6 +14,8 @@ class EFLGames(object):
         session = orm.Session(bind=engine.connect())
         # Build the game query
         gamequery = session.query(orm.Game)
+        if (seasonid is not None) or (leagueid is not None):
+            print("WARNING: League and season filter not implemented.")
         if startdate is not None:
             gamequery = gamequery.filter(orm.Game.date >= startdate)
         if enddate is not None:
@@ -27,24 +27,29 @@ class EFLGames(object):
                 + [g.awayteam for g in self.games]
                 ))
         session.close()
-
-    def to_dataframe(self):
-        """Output the games contained as a pandas DataFrame."""
-        raise NotImplementedError()
-
+    
     def addteam(self, teamid, exist_ok=True):
         """Adds a team to this object. Useful for when a team is in the league
         but hasn't played any games yet in the season. If exist_ok is false, 
         this method will raise an exception."""
-        session = 
+        session = orm.Session(bind=engine.connect())
+        team = session.query(orm.Team)\
+                .filter(orm.Team.id == teamid)\
+                .one_or_none()
+        if team is None:
+            raise Exception('Team ID {} does not exist.'.format(teamid))
+        if (not exist_ok) and team in self.teams:
+            raise Exception('Team ID {} already in data.'.format(teamid))
+        self.teams = list(set(self.teams + [team]))
 
 
 def seasonid(start_year):
     """Return a unique seasonid from the database based on the season's start
     year."""
     session = orm.Session(bind=engine.connect())
-    season = orm.query(orm.Season).filter(orm.Season.start == start_year).\
-            one_or_none()
+    season = session.query(orm.Season)\
+            .filter(orm.Season.start == start_year)\
+            .one_or_none()
     if season is None:
         return(None)
     else:
@@ -54,10 +59,22 @@ def leagueid(short_name):
     """Return a unique leagueid from the database based on the league's short
     name."""
     session = orm.Session(bind=engine.connect())
-    league = orm.query(orm.League).filter(orm.League.shortname == short_name).\
-            one_or_none()
+    league = session.query(orm.League)\
+            .filter(orm.League.shortname == short_name)\
+            .one_or_none()
     if league is None:
         return(None)
     else:
         return(league.id)
     
+def teamid(short_name):
+    """Return a unique teamid from the database based on the team's short
+    name."""
+    session = orm.Session(bind=engine.connect())
+    team = session.query(orm.Team)\
+            .filter(orm.Team.shortname == short_name)\
+            .one_or_none()
+    if team is None:
+        return(None)
+    else:
+        return(team.id)
