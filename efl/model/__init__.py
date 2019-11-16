70a8166a2cacc3eb77c9905e710dbc09daebd1c4
Ian Taylor <ian@iantaylor.xyz>
2019-11-16 12:56:59 -0700

    Added prior as importable at package level

--- efl/model/__init__.py
+++ efl/model/__init__.py
@@ -1,4 +1,4 @@
 from .symordreg          import EFLSymOrdReg, EFLSymOrdReg_Prior
 from .poisregnumberphile import EFLPoisRegNumberphile
-from .poisregsimple      import EFLPoisRegSimple
+from .poisregsimple      import EFLPoisRegSimple, EFLPoisRegSimple_Prior
 from .poisreghier        import EFLPoisRegHier
