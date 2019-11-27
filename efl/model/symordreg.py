e6e37e0357cb095bd03ca4aea1c1a420d69f1c57
Ian Taylor <ian@iantaylor.xyz>
2019-11-27 09:48:32 -0500

    Added EFLSymOrdMulReg class for multiplicative matchup effects.

--- efl/model/symordreg.py
+++ efl/model/symordreg.py
@@ -11,6 +11,12 @@ from . import base
 import numpy
 
 
+############################################################
+## EFLSymOrdReg ############################################
+## Simplest Symmetric Ordinal Regression Model #############
+############################################################
+
+
 class EFLSymOrdReg(base.EFL_ResultModel):
     """*Sym*metric *Ord*inal *Reg*ression model for EFL data."""
     
@@ -54,7 +60,92 @@ class EFLSymOrdReg(base.EFL_ResultModel):
                 self._modeldata['theta_prior_loc'], 
                 self._modeldata['theta_prior_scale']))
         return {'teams_raw':teams_raw, 'home':home, 'theta':theta}
+
+
+############################################################
+## EFLSymOrdMulReg #########################################
+## Symmetric Ordinal Regression with Multiplicative Effects
+############################################################
+
+
+class EFLSymOrdMulReg(base.EFL_ResultModel):
+    """*Sym*metric *Ord*inal *Mul*tiplicative *Reg*ression model for EFL data."""
     
+    def __init__(self, eflgames, prior=None, muldim=1, **kwargs):
+        """Parameters:
+            eflgames - an EFLGames instance
+            prior - an EFLSymOrdReg_Prior instance, or None for diffuse priors
+            muldim - the dimension of the multiplicative effects
+            **kwargs - extra arguments passed to base models (usually Stan
+                sampling options)
+        """
+        team_names = [t.shortname for t in eflgames.teams]
+        # Create priors
+        if prior is None:
+            prior = EFLSymOrdReg_Prior.default_prior(team_names)
+        # Create parameter mapping
+        efl2stan = {'DrawBoundary':'theta', 'HomeField':'home'}
+        for i,t in enumerate(team_names):
+            efl2stan[t] = 'teams[{}]'.format(i+1)
+        pargroups = {'teams':team_names}
+        matchups = []
+        for i1,t1 in enumerate(team_names):
+            teampars = []
+            for i2,t2 in enumerate(team_names):
+                if i1 != i2:
+                    parname = '{} vs {}'.format(t1,t2)
+                    efl2stan[parname] = 'matchup[{},{}]'.format(i1+1,i2+1)
+                    teampars.append(parname)
+                    matchups.append(parname)
+            pargroups['{} vs'.format(t1)] = teampars
+        pargroups['matchups'] = matchups
+        scales = []
+        for i in range(muldim):
+            parname = 'mulscale[{}]'.format(i+1)
+            efl2stan[parname] = 'uvscale[{}]'.format(i+1)
+            scales.append(parname)
+        pargroups['mulscale'] = scales
+        # Call super init
+        super().__init__(
+                modelfile      = 'symordreg_mult',
+                eflgames       = eflgames,
+                extramodeldata = {'uvdim':muldim, 
+                                  **prior.get_params(team_names)},
+                efl2stan       = efl2stan,
+                pargroups      = pargroups,
+                **kwargs)
+    
+    def _stan_inits(self, chain_id=None):
+        """Draw from a multivariate normal distribution and a logistic
+        distribution to produce prior values for beta and theta."""
+        P = self._modeldata['teams_prior_mean'].shape[0]
+        K = self._modeldata['uvdim']
+        teams = numpy.random.multivariate_normal(
+                self._modeldata['teams_prior_mean'], 
+                self._modeldata['teams_prior_var'])
+        teams_raw = (teams - teams.mean())[:(P-1)]
+        home = numpy.random.normal(
+                self._modeldata['home_prior_mean'],
+                self._modeldata['home_prior_sd'])
+        theta = abs(numpy.random.logistic(
+                self._modeldata['theta_prior_loc'], 
+                self._modeldata['theta_prior_scale']))
+        uvscale = numpy.array(sorted(abs(numpy.random.normal(size=K))))
+        uvcorr = numpy.identity(2*K)
+        UV = numpy.random.multivariate_normal(
+                numpy.zeros(2*K),
+                numpy.diag(numpy.append(uvscale,uvscale)),
+                size=P)
+        return {'teams_raw':teams_raw, 'home':home, 'theta':theta,
+                'uvscale':uvscale, 'uvcorr_chol':uvcorr,
+                'U':UV[:,:K], 'V':UV[:,K:]}
+
+
+############################################################
+## EFLSymOrdReg_Prior ######################################
+## Prior for either of the above models ####################
+############################################################
+
 
 class EFLSymOrdReg_Prior(object):
     """A class holding a prior for the EFLSymOrdReg model."""
