418d3dd5b251a9e79e6ede9a2ceade85887610d6
Ian Taylor <ian@iantaylor.xyz>
2019-12-29 15:38:49 -0500

    Added SOR HTI model to __init__, removed SymOrdMulReg

--- efl/model/__init__.py
+++ efl/model/__init__.py
@@ -1,4 +1,5 @@
-from .symordreg          import EFLSymOrdReg, EFLSymOrdMulReg, EFLSymOrdReg_Prior
+from .symordreg          import EFLSymOrdReg, EFLSymOrdReg_Prior
+from .symordreghti       import EFLSymOrdRegHTI, EFLSymOrdRegHTI_Prior
 from .poisregnumberphile import EFLPoisRegNumberphile, EFLPoisRegNumberphile_Prior
 from .poisregsimple      import EFLPoisRegSimple, EFLPoisRegSimple_Prior
 from .poisreghier        import EFLPoisRegHier, EFLPoisRegHier_Prior
