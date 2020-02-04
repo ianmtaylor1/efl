d9f75e9aa95057faed27407132b587a0ae74ed80
Ian Taylor <ian@iantaylor.xyz>
2020-02-04 08:14:36 -0700

    Created poisson regression model with Gaussian copula.

--- efl/model/__init__.py
+++ efl/model/__init__.py
@@ -1,3 +1,5 @@
+# These models work and are reasonably fast for what they do.
+# Considered the "main suite" of models.
 from .symordreg          import SymOrdReg,          SymOrdReg_Prior
 from .symordreghti       import SymOrdRegHTI,       SymOrdRegHTI_Prior
 from .poisregnumberphile import PoisRegNumberphile, PoisRegNumberphile_Prior
@@ -5,5 +7,6 @@ from .poisregsimple      import PoisRegSimple,      PoisRegSimple_Prior
 from .consuljainreg      import ConsulJainReg,      ConsulJainReg_Prior
 from .poisregefgm        import PoisRegEFGM,        PoisRegEFGM_Prior
 
-# This is technically working, but VERY slow. Not recommended
+# These models are working, but VERY slow. Not recommended
 from .compoisreg         import COMPoisReg,         COMPoisReg_Prior
+from .poisreggc          import PoisRegGC,          PoisRegGC_Prior
