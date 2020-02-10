4d49bb4d565f84c4807e3d88ff6d2e35e0fa4a27
Ian Taylor <ian@iantaylor.xyz>
2020-02-10 14:51:32 -0700

    Added consuljainregefgm to package level imports

--- efl/model/__init__.py
+++ efl/model/__init__.py
@@ -6,6 +6,7 @@ from .poisregsimple      import PoisRegSimple,      PoisRegSimple_Prior
 from .poisregnumberphile import PoisRegNumberphile, PoisRegNumberphile_Prior
 from .consuljainreg      import ConsulJainReg,      ConsulJainReg_Prior
 from .poisregefgm        import PoisRegEFGM,        PoisRegEFGM_Prior
+from .consuljainregefgm  import ConsulJainRegEFGM,  ConsulJainRegEFGM_Prior
 
 # Subpackage for experimental models (in development, interesting but slow,
 # or just work that I did that would be a shame to trash but isn't very
